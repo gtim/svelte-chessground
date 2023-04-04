37a67c0675c1370687e33bba00af58c1382664b3
Tim N <tim@gurka.se>
2023-04-04 13:22:27 +0200

    chessground with default styling

---
+++ src/lib/Chessground.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Chessground } from 'chessground';
+
+	import './assets/chessground.base.css';
+	import './assets/chessground.brown.css';
+	import './assets/chessground.cburnett.css';
+
+	import { onMount } from 'svelte';
+
+	let config = {};
+	let cg;
+	let div;
+
+	onMount(async () => {
+		cg = Chessground( div, config );
+	});
+</script>
+
+<div id="chessground" bind:this={div}>
+</div>
+
+<style>
+	#chessground {
+		width:100%;
+		height:100%;
+	}
+</style>
