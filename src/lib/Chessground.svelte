4f436e026d559c95e4dbfe0e18ea43ae48aeb5a1
Tim N <tim@gurka.se>
2023-04-06 13:39:14 +0200

    reactive props for every simple-valued Chessground parameter

--- src/lib/Chessground.svelte
+++ src/lib/Chessground.svelte
@@ -9,19 +9,79 @@
 	import { onMount } from 'svelte';
 
 
+	/**
+	 *
+	 * Exported props.
+	 * All exported props are reactive and can be set directly or via the config object.
+	 * Cannot be bound, use callback functions via the "events" objects to monitor state.
+	 *
+	 * All simple-valued Chessground parameters have their own props. The others must be set via the config prop and are:
+	 *     highlight, animation, movable, premovable, predroppable, draggable, selectable, events, drawable
+	 * 
+	 */
 
-	/** @type {import('chessground/types').FEN} */
-	export let fen = initialFen;
+	/** @type {import('chessground/types').FEN | undefined} */
+	export let fen = undefined;
 	$: setConfig( { fen: fen } );
 
-	/** @type {import('chessground/types').Color} */
-	export let orientation = 'white';
+	/** @type {import('chessground/types').Color | undefined} */
+	export let orientation = undefined;
 	$: setConfig( { orientation: orientation } );
+
+	/** @type {import('chessground/types').Color | undefined} */
+	export let turnColor = undefined;
+	$: setConfig( { turnColor: turnColor } );
+
+	/** @type {import('chessground/types').Color | boolean | undefined} */
+	export let check = undefined;
+	$: setConfig( { check: check } );
+
+	/** @type {import('chessground/types').Key[] | undefined} */
+	export let lastMove = undefined;
+	$: setConfig( { lastMove: lastMove } );
+
+	/** @type {import('chessground/types').Key | undefined} */
+	export let selected = undefined;
+	$: setConfig( { selected: selected } );
+
+	/** @type {boolean | undefined} */
+	export let coordinates = undefined;
+	$: setConfig( { coordinates: coordinates } );
+
+	/** @type {boolean | undefined} */
+	export let autoCastle = undefined;
+	$: setConfig( { autoCastle: autoCastle } );
+
+	/** @type {boolean | undefined} */
+	export let viewOnly = undefined;
+	$: setConfig( { viewOnly: viewOnly } );
+
+	/** @type {boolean | undefined} */
+	export let disableContextMenu = undefined;
+	$: setConfig( { disableContextMenu: disableContextMenu } );
+
+	/** @type {boolean | undefined} */
+	export let addPieceZIndex = undefined;
+	$: setConfig( { addPieceZIndex: addPieceZIndex } );
+
+	/** @type {HTMLElement | undefined} */
+	export let addDimensionsCssVarsTo = undefined;
+	$: setConfig( { addDimensionsCssVarsTo: addDimensionsCssVarsTo } );
+
+	/** @type {boolean | undefined} */
+	export let blockTouchScroll = undefined;
+	$: setConfig( { blockTouchScroll: blockTouchScroll } );
 
 	/** @type {import('chessground/config').Config} */
 	export let config = {};
+	$: setConfig( config );
 
 
+	/**
+	 *
+	 * 
+	 * 
+	 */
 
 	/** @type {HTMLDivElement} */
 	let container;
@@ -30,8 +90,19 @@
 	let chessground;
 
 	onMount(async () => {
-		config.orientation ??= orientation;
-		config.fen ??= fen;
+		if ( orientation            !== undefined ) config.orientation = orientation;
+		if ( fen                    !== undefined ) config.fen = fen;
+		if ( turnColor              !== undefined ) config.turnColor = turnColor;
+		if ( check                  !== undefined ) config.check = check;
+		if ( lastMove               !== undefined ) config.lastMove = lastMove;
+		if ( selected               !== undefined ) config.selected = selected;
+		if ( coordinates            !== undefined ) config.coordinates = coordinates ;
+		if ( autoCastle             !== undefined ) config.autoCastle = autoCastle;
+		if ( viewOnly               !== undefined ) config.viewOnly = viewOnly;
+		if ( disableContextMenu     !== undefined ) config.disableContextMenu = disableContextMenu;
+		if ( addPieceZIndex         !== undefined ) config.addPieceZIndex = addPieceZIndex;
+		if ( addDimensionsCssVarsTo !== undefined ) config.addDimensionsCssVarsTo = addDimensionsCssVarsTo;
+		if ( blockTouchScroll       !== undefined ) config.blockTouchScroll = blockTouchScroll;
 		chessground = Chessground( container, config );
 	});
 
